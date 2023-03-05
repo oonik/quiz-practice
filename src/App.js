629ceff0d5af703aeae6e2af1e9d904a045d1e62
oonik <aniksarkar0177@gmail.com>
2023-03-05 17:06:52 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './layout/Main';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Topic from './components/Topic/Topic';
+import Blog from './components/Blog/Blog';
+import About from './components/About/About';
 
 function App() {
+  const router = createBrowserRouter([
+   {
+    path: '/',
+    element: <Main></Main>,
+    children:[
+      {
+        path:'/',
+        element: <Header></Header>
+      },
+      {
+        path:'/home',
+        element:<Home></Home>
+      },
+      {
+        path:'/topic',
+        element: <Topic></Topic>
+      },
+      {
+        path:'/blog',
+        element: <Blog></Blog>
+      },
+      {
+        path:'/about',
+        element: <About></About>
+      }
+    ]
+   }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel tempore asperiores corporis vero tempora magni eum ipsum ex, quis incidunt minima consequatur enim expedita dignissimos explicabo aperiam et consectetur fugit?</p>
+        </div>
+    );
+};
+
+export default Header;
